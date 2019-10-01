68ae79ba70a17138e9599828048b8b460297deed
Mark Halls <mark.halls@gmail.com>
2019-10-01 14:02:41 -0700

    Add hover events for nav

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+const navLinks = document.querySelectorAll("nav a");
+
+navLinks.forEach(link => {
+  link.addEventListener("mouseenter", () => {
+    link.style["font-size"] = "3rem";
+    link.style["transition-duration"] = "0.3s";
+  });
+  link.addEventListener("mouseleave", () => {
+    link.style["font-size"] = "1.6rem";
+  });
+});
